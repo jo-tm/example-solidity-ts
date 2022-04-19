5793b1bd38904ac0781fe0dc0b6232fd3999b4dc
joigno <30682875+joigno@users.noreply.github.com>
2022-04-19 12:10:22 +0200

    empty contract

---
+++ contracts/DelayedJobs.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+
+contract DelayedJobs is Ownable {
+    using SafeMath for uint;
+
+}
