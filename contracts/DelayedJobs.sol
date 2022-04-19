1d2d458ce8759b29653b19b58797abf009cb518e
joigno <30682875+joigno@users.noreply.github.com>
2022-04-19 14:04:06 +0200

    part 2

--- contracts/DelayedJobs.sol
+++ contracts/DelayedJobs.sol
@@ -15,6 +15,8 @@ contract DelayedJobs is Ownable {
     uint public delay;
     mapping (bytes32 => bool) public submittedTxs;
     mapping (bytes32 => uint256) public submittedTimes;
+    mapping (bytes32 => uint256) public submittedBestBid;
+    mapping (bytes32 => address) public submittedBestBidder;
 
     uint public constant MIN_DELAY = 1 hours;
     uint public constant MAX_DELAY = 48 hours;
@@ -85,5 +87,100 @@ contract DelayedJobs is Ownable {
 
             return returnData;
         }
-        
+
+    function submitJobAuction(address target, string memory signature, bytes memory data, uint256 timeout) public payable returns (bytes32) {
+            require(msg.sender == userA, "DelayedJobs::submitJobAuction: Call must come from userA.");
+            require(msg.value > 0, "DelayedJobs::submitJobAuction: Must include an ETH reward.");
+            require(timeout > MIN_DELAY, "DelayedJobs::submitJobAuction: Timeout must be great than 1 hour");
+
+            // now msg.value sent by userA is the maximum reward to be paid.
+            bytes32 txHash = keccak256(abi.encode(target, msg.value, signature, data, timeout));
+            submittedTxs[txHash] = true;
+            submittedTimes[txHash] = block.timestamp;
+            submittedBestBid[txHash] = msg.value;
+
+            emit JobSubmitted(txHash, target, msg.value, signature, data);
+            return txHash;
+        }
+
+    function placeJobBid(address target, uint maxBid, uint bid, string memory signature, bytes memory data, uint256 timeout) public payable returns (bytes32) {
+            require(msg.sender != userA, "DelayedJobs::placeJobBid: Call must not come from userA.");
+
+            bytes32 txHash = keccak256(abi.encode(target, maxBid, signature, data, timeout));
+            require(submittedTxs[txHash], "DelayedJobs::placeJobBid: Transaction has not been submitted.");
+            require(block.timestamp < submittedTimes[txHash]+delay, "DelayedJobs::placeJobBid: Transaction cannot surpass delay time.");
+            require(submittedBestBid[txHash] > bid, "DelayedJobs::placeJobBid: Bid must be positive and smaller than best bid" );
+            require(msg.value == maxBid - bid, "DelayedJobs::placeJobBid: Collateral must be diff with max bid." );
+            
+            if (submittedBestBid[txHash] < maxBid) {
+                // refund previous bidder if prev best exists
+                uint256 prevCollateral = maxBid - submittedBestBid[txHash];
+                (bool success, ) = submittedBestBidder[txHash].call{value: prevCollateral}("");
+                require( success, "refund failed");
+            }
+            submittedBestBid[txHash] = bid;
+            submittedBestBidder[txHash] = msg.sender;
+
+            return txHash;
+        }
+
+    function executeJobBid(address target, uint maxBid, string memory signature, bytes memory data, uint256 timeout) public returns (bytes memory) {
+            require(msg.sender != userA, "DelayedJobs::executeJobBid: Call must not come from userA.");
+
+            bytes32 txHash = keccak256(abi.encode(target, maxBid, signature, data, timeout));
+            require(submittedTxs[txHash], "DelayedJobs::executeJobBid: Transaction hasn't been submitted.");
+            require(block.timestamp < submittedTimes[txHash]+delay+timeout, "DelayedJobs::executeJobBid: Transaction has surpassed delay+timeout time.");
+            require(block.timestamp >= submittedTimes[txHash]+delay, "DelayedJobs::executeJobBid: Transaction has not surpassed delay time.");
+            require(submittedBestBidder[txHash] == msg.sender, "DelayedJobs::executeJobBid: Sender is best bidder." );
+            
+            submittedTxs[txHash] = false;
+            submittedTimes[txHash] = 0;
+            
+            // refund bidder collateral + bid = (maxBid-bid) + bid = maxBid
+            (bool success, ) = submittedBestBidder[txHash].call{value: maxBid}("");
+            require( success, "transfer reward failed");
+            // refund userA (maxBid-bid)
+            (bool success_, ) = userA.call{value: maxBid-submittedBestBid[txHash]}("");
+            require( success_, "transfer diff to userA failed");
+
+            submittedBestBid[txHash] = 0;
+            submittedBestBidder[txHash] = address(0);
+
+            bytes memory callData;
+
+            if (bytes(signature).length == 0) {
+                callData = data;
+            } else {
+                callData = abi.encodePacked(bytes4(keccak256(bytes(signature))), data);
+            }
+
+            // solium-disable-next-line security/no-call-value
+            (bool _success, bytes memory returnData) = target.call(callData);
+            require(_success, "DelayedJobs::executeJobBid: Transaction execution reverted.");
+
+            return returnData;
+        }
+
+    function cancelJobAuction(address target, uint256 maxBid, string memory signature, bytes memory data, uint256 timeout) public returns (bytes32) {
+            require(msg.sender == userA, "DelayedJobs::submitJobAuction: Call must come from userA.");
+
+            bytes32 txHash = keccak256(abi.encode(target, maxBid, signature, data, timeout));
+            require(submittedTxs[txHash], "DelayedJobs::executeJobBid: Transaction hasn't been submitted or already executed.");
+            require(block.timestamp >= submittedTimes[txHash]+delay+timeout, "DelayedJobs::cancelJobAuction: Cancelling only after delay+timeout");
+            
+            submittedTxs[txHash] = false;
+            submittedTimes[txHash] = 0;
+            submittedBestBid[txHash] = 0;
+            submittedBestBidder[txHash] = address(0);
+
+            // refund userA (maxBid)
+            (bool success, ) = userA.call{value: maxBid}("");
+            require( success, "transfer total reward back to userA failed");
+
+            return txHash;
+        }
+
+
+
+
 }
--- test/delayedjobs.test.ts
+++ test/delayedjobs.test.ts
@@ -14,6 +14,8 @@ import { Wallet, BigNumber, constants, utils, ethers, providers } from 'ethers'
 // Wires up Waffle with Chai
 chai.use(solidity)
 
+const oneWeekInSeconds = 7 * 24 * 60 * 60 // etherUnsigned(7 * 24 * 60 * 60);
+
 async function mineBlock(wallet: SignerWithAddress) {
     return wallet.sendTransaction({to: constants.AddressZero, value: 1})
 }
@@ -108,6 +110,193 @@ describe('DelayedJobs', () => {
             expect(receipt2.logs.length).equals(1)
             // userB gets rewards 1 ether, minus gas consumed.
             expect((await userB.getBalance()).gte( initBalance.add(reward).sub(BigNumber.from('47780257489784')) )).equals(true)
+        })
+    })
+
+    describe('submitJobAuction()', () => {
+        it('not userB', async () => {
+            const timeout = BigNumber.from(3601)
+            await expect(jobs.connect(userB).submitJobAuction(target, signature, data, timeout, {value:reward})).to.be.revertedWith(
+                 'DelayedJobs::submitJobAuction: Call must come from userA.'
+            )
+        })
+
+        it('timeout too small', async () => {
+            const timeout = BigNumber.from(3600)
+            await expect(jobs.connect(userA).submitJobAuction(target, signature, data, timeout, {value:reward})).to.be.revertedWith(
+                 'DelayedJobs::submitJobAuction: Timeout must be great than 1 hour'
+            )
+        })
+
+        it('userA ok', async () => {
+            const timeout = BigNumber.from(3601)
+            const receipt = await successfulTransaction(jobs.connect(userA).submitJobAuction(target, signature, data, timeout, {value:reward}))
+            expect(receipt.logs.length).equals(1)
+        })
+    })
+
+    describe('placeJobBid()', () => {
+        it('not userA', async () => {
+            const timeout = BigNumber.from(3601)
+            const receipt = await successfulTransaction(jobs.connect(userA).submitJobAuction(target, signature, data, timeout, {value:reward}))
+            expect(receipt.logs.length).equals(1)
+            const bid = reward.sub(utils.parseUnits('10', 'gwei'))
+            const diff = reward.sub(bid)
+            await expect(jobs.connect(userA).placeJobBid(target, reward, bid, signature, data, timeout, {value:diff})).to.be.revertedWith(
+                 'DelayedJobs::placeJobBid: Call must not come from userA.'
+            )
+        })
+
+        it('bid too large', async () => {
+            const timeout = BigNumber.from(3601)
+            const receipt = await successfulTransaction(jobs.connect(userA).submitJobAuction(target, signature, data, timeout, {value:reward}))
+            expect(receipt.logs.length).equals(1)
+            const bid = reward
+            const diff = reward.sub(bid)
+            await expect(jobs.connect(userB).placeJobBid(target, reward, bid, signature, data, timeout, {value:diff})).to.be.revertedWith(
+                 'DelayedJobs::placeJobBid: Bid must be positive and smaller than best bid'
+            )
+        })
+        it('too late after delay', async () => {
+            const timeout = BigNumber.from(3601)
+            const receipt = await successfulTransaction(jobs.connect(userA).submitJobAuction(target, signature, data, timeout, {value:reward}))
+            expect(receipt.logs.length).equals(1)
+            await increaseTime(userB, delay.toNumber())
+            const bid = reward
+            const diff = reward.sub(bid)
+            await expect(jobs.connect(userB).placeJobBid(target, reward, bid, signature, data, timeout, {value:diff})).to.be.revertedWith(
+                 'DelayedJobs::placeJobBid: Transaction cannot surpass delay time.'
+            )
+        })
+        it('unsubmitted tx', async () => {
+            const timeout = BigNumber.from(3601)
+            const bid = reward.sub(utils.parseUnits('10', 'gwei'))
+            const diff = reward.sub(bid)
+            await expect(jobs.connect(userB).placeJobBid(target, reward, bid, signature, data, timeout, {value:diff})).to.be.revertedWith(
+                 'DelayedJobs::placeJobBid: Transaction has not been submitted.'
+            )
+        })
+        it('good bid', async () => {
+            const timeout = BigNumber.from(3601)
+            const receipt = await successfulTransaction(jobs.connect(userA).submitJobAuction(target, signature, data, timeout, {value:reward}))
+            expect(receipt.logs.length).equals(1)
+            //await increaseTime(userB, delay.toNumber() - 1)
+            const bid = reward.sub(utils.parseUnits('10', 'gwei'))
+            const diff = reward.sub(bid)
+            await expect(jobs.connect(userB).placeJobBid(target, reward, bid, signature, data, timeout, {value:diff})).to.be.not.revertedWith(
+                'DelayedJobs::placeJobBid: Transaction has not been submitted.'
+            ) 
+            // const receipt2 = await successfulTransaction(jobs.connect(userB).placeJobBid(target, reward, bid, signature, data, timeout, {value:diff}))
+            // expect(receipt2.logs.length).equals(1)
+        })
+    })
+
+    describe('executeJobBid()', () => {
+        it('not userA', async () => {
+            const timeout = BigNumber.from(3601)
+            const receipt = await successfulTransaction(jobs.connect(userA).submitJobAuction(target, signature, data, timeout, {value:reward}))
+            expect(receipt.logs.length).equals(1)
+            //await increaseTime(userB, delay.toNumber() - 1)
+            const bid = reward.sub(utils.parseUnits('10', 'gwei'))
+            const diff = reward.sub(bid)
+            await expect(jobs.connect(userB).placeJobBid(target, reward, bid, signature, data, timeout, {value:diff})).to.be.not.revertedWith(
+                'DelayedJobs::placeJobBid: Transaction has not been submitted.'
+            ) 
+            await increaseTime(userB, delay.toNumber() + 1)
+            await expect(jobs.connect(userA).executeJobBid(target, reward, signature, data, timeout)).to.be.revertedWith(
+                'DelayedJobs::executeJobBid: Call must not come from userA.'
+           )
+        })
+        it('too late', async () => {
+            const timeout = BigNumber.from(3601)
+            const receipt = await successfulTransaction(jobs.connect(userA).submitJobAuction(target, signature, data, timeout, {value:reward}))
+            expect(receipt.logs.length).equals(1)
+            //await increaseTime(userB, delay.toNumber() - 1)
+            const bid = reward.sub(utils.parseUnits('10', 'gwei'))
+            const diff = reward.sub(bid)
+            await expect(jobs.connect(userB).placeJobBid(target, reward, bid, signature, data, timeout, {value:diff})).to.be.not.revertedWith(
+                'DelayedJobs::placeJobBid: Transaction has not been submitted.'
+            ) 
+            await increaseTime(userB, delay.toNumber() + timeout.toNumber() + 1)
+            await expect(jobs.connect(userB).executeJobBid(target, reward, signature, data, timeout)).to.be.revertedWith(
+                'DelayedJobs::executeJobBid: Transaction has surpassed delay+timeout time.'
+           )
+        })
+        it('too early', async () => {
+            const timeout = BigNumber.from(3601)
+            const receipt = await successfulTransaction(jobs.connect(userA).submitJobAuction(target, signature, data, timeout, {value:reward}))
+            expect(receipt.logs.length).equals(1)
+            //await increaseTime(userB, delay.toNumber() - 1)
+            const bid = reward.sub(utils.parseUnits('10', 'gwei'))
+            const diff = reward.sub(bid)
+            await expect(jobs.connect(userB).placeJobBid(target, reward, bid, signature, data, timeout, {value:diff})).to.be.not.revertedWith(
+                'DelayedJobs::placeJobBid: Transaction has not been submitted.'
+            ) 
+            await increaseTime(userB, delay.toNumber() - 100)
+            await expect(jobs.connect(userB).executeJobBid(target, reward, signature, data, timeout)).to.be.revertedWith(
+                'DelayedJobs::executeJobBid: Transaction has not surpassed delay time.'
+           )
+        })
+        it('execution', async () => {
+            const timeout = BigNumber.from(3601)
+            const receipt = await successfulTransaction(jobs.connect(userA).submitJobAuction(target, signature, data, timeout, {value:reward}))
+            expect(receipt.logs.length).equals(1)
+            const bid = reward.sub(utils.parseUnits('10', 'gwei'))
+            const diff = reward.sub(bid)
+            await expect(jobs.connect(userB).placeJobBid(target, reward, bid, signature, data, timeout, {value:diff})).to.be.not.revertedWith(
+                'DelayedJobs::placeJobBid: Transaction has not been submitted.'
+            ) 
+            await increaseTime(userB, delay.toNumber() + 1)
+            await expect(jobs.connect(userB).executeJobBid(target, reward, signature, data, timeout)).to.be.not.revertedWith(
+                'DelayedJobs::executeJobBid: Transaction execution reverted.'
+           )
+        })
+    })
+
+    describe('cancelJobAuction()', () => {
+        it('only userA', async () => {
+            const timeout = BigNumber.from(3601)
+            const receipt = await successfulTransaction(jobs.connect(userA).submitJobAuction(target, signature, data, timeout, {value:reward}))
+            expect(receipt.logs.length).equals(1)
+            //await increaseTime(userB, delay.toNumber() - 1)
+            const bid = reward.sub(utils.parseUnits('10', 'gwei'))
+            const diff = reward.sub(bid)
+            await expect(jobs.connect(userB).placeJobBid(target, reward, bid, signature, data, timeout, {value:diff})).to.be.not.revertedWith(
+                'DelayedJobs::placeJobBid: Transaction has not been submitted.'
+            ) 
+            await increaseTime(userB, delay.toNumber() + 1)
+            await expect(jobs.connect(userB).cancelJobAuction(target, reward, signature, data, timeout)).to.be.revertedWith(
+                'DelayedJobs::submitJobAuction: Call must come from userA.'
+           )
+        })
+        it('too early', async () => {
+            const timeout = BigNumber.from(3601)
+            const receipt = await successfulTransaction(jobs.connect(userA).submitJobAuction(target, signature, data, timeout, {value:reward}))
+            expect(receipt.logs.length).equals(1)
+            //await increaseTime(userB, delay.toNumber() - 1)
+            const bid = reward.sub(utils.parseUnits('10', 'gwei'))
+            const diff = reward.sub(bid)
+            await expect(jobs.connect(userB).placeJobBid(target, reward, bid, signature, data, timeout, {value:diff})).to.be.not.revertedWith(
+                'DelayedJobs::placeJobBid: Transaction has not been submitted.'
+            ) 
+            await increaseTime(userB, delay.toNumber())
+            await expect(jobs.connect(userA).cancelJobAuction(target, reward, signature, data, timeout)).to.be.revertedWith(
+                'DelayedJobs::cancelJobAuction: Cancelling only after delay+timeout'
+           )
+        })
+        it('cancelled', async () => {
+            const timeout = BigNumber.from(3601)
+            const receipt = await successfulTransaction(jobs.connect(userA).submitJobAuction(target, signature, data, timeout, {value:reward}))
+            expect(receipt.logs.length).equals(1)
+            const bid = reward.sub(utils.parseUnits('10', 'gwei'))
+            const diff = reward.sub(bid)
+            await expect(jobs.connect(userB).placeJobBid(target, reward, bid, signature, data, timeout, {value:diff})).to.be.not.revertedWith(
+                'DelayedJobs::placeJobBid: Transaction has not been submitted.'
+            ) 
+            await increaseTime(userB, delay.toNumber() + timeout.toNumber() + 1)
+            await expect(jobs.connect(userA).cancelJobAuction(target, reward, signature, data, timeout)).to.be.not.revertedWith(
+                'DelayedJobs::executeJobBid: Transaction execution reverted.'
+            )
         })
     })
 
